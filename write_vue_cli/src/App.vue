e9a2af620af28957cf9e52f9e2ba2693477f513c
HelloWorldZzc <94695853+HelloWorldZzc@users.noreply.github.com>
2022-07-07 10:53:58 +0800

    Add files via upload

---
+++ write_vue_cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="title">hello app</h1>
+    <home></home>
+  </div>
+</template>
+
+<script>
+
+import Home from "./home/index.vue"
+export default {
+  name: "App",
+  components:{
+    Home
+  }
+
+};
+</script>
+
+<style>
+.title {
+  color: pink;
+}
+</style>
---
+++ write_vue_cli/src/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <hr/>
+  <h2>这是home组件</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
